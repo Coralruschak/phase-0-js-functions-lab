269d57de09bf95401a4174b5daa91985b8ce07fc
Coral <coralruschak@gmail.com>
2025-06-08 01:44:01 -0400

    All tests passing

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+function calculateTax(amount) {
+    return (amount * .1)
+}
+
+function convertToUpperCase(text) {
+    return text.toUpperCase()
+}
+
+function findMaximum(num1, num2) {
+    if (num1>num2) {
+        return num1
+    }
+    else {
+        return num2
+    }
+}
 
 
+function isPalindrome(word) {
+    let newWord = word.split('').reverse().join('')
+    if (word === newWord) {
+       return true
+    }
+    else {
+       return false
+    }
 
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    return (originalPrice - (originalPrice * (discountPercentage/100)))
+}
 
 
 // This is required for the test to function properly  
